7a9be2ce1f912ceb9f77b7155e6cdf7b4732ca91
guslopezr <guslopezr@gmail.com>
2022-10-17 12:51:13 -0300

    nueva versión fotos naturales

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import "./styles.css";
+import axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./views/Home";
+import Favoritos from "./views/Favoritos";
+import { useEffect } from "react";
+import useImages from "./hooksimages/useImages";
+
+export default function App() {
+  const { setImagenes } = useImages();
+
+  useEffect(() => {
+    async function getImagenes() {/* 
+      const endpoint = "/fotos.json"; */
+//      const endpoint = "http://demo0670365.mockable.io/naturalpics";
+
+/*       const url = `http://localhost:3000/${endpoint}`;
+ */      const url = `http://demo0670365.mockable.io/naturalpics`;
+
+      
+      const resultado = await axios(url);
+      setImagenes(resultado.data.photos);
+    }
+
+    getImagenes();
+  }, []);
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favoritos" element={<Favoritos />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
---
+++ src/views/Home.js
@@ -0,0 +1,12 @@
+import Galeria from "../components/Galeria";
+
+export default function Home() {
+  return (
+    <div id="Home">
+      <h1>Fotos naturales</h1>
+      <h5>Selecciona tus fotos favoritas</h5>
+
+      <Galeria />
+    </div>
+  );
+}
